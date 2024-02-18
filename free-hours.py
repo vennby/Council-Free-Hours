d37c39e90bdaa1722c5c3c1ae77dff921c05d904
vennela vallabhaneni <128285228+vennby@users.noreply.github.com>
2024-02-18 22:06:11 +0400

    Update and rename test.py to free-hours.py

--- free-hours.py
+++ free-hours.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+lit.set_page_config(page_title = "Free Hours", page_icon=":bulb:")
+
 hours = {
     "M1": [0,0,0,0,0,1,0,1,0,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1],
     "M2": [0,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
